6241b400e3edb7343fb0a02c4e052cb40578782b
Pranshu Agrawal <43396684+IMPranshu@users.noreply.github.com>
2021-11-13 16:37:09 +0530

    Add files via upload

--- python_code/main.py
+++ python_code/main.py
@@ -1,8 +1,12 @@
+import pandas as pd
 from load_data import *
 from print_data import *
 
-# List of the Required Countries
-country_list = ["NYC", "USA", "IND", "PHIL", "AU"]
+# List of Countries
+data = pd.read_csv(
+    "C:/Users/Specter/Desktop/Incubyte/python_code/country-codes.csv")
+country_list = data['Alpha-3 code'].values
+
 
 for country in country_list:
     print_data(country)
